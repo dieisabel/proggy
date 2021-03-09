ac8e91e20fc78155d8c8beccc45d7be466ffca98
dieisabel <konstkadyr@gmail.com>
2021-03-09 16:07:58 +0200

    Добавил маршрут для userpage в blog/urls.py
    В карту маршрутов приложения blog urls.py добавил маршрут к вьюшке
    userpage

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blogs),
-    path('bloggers', views.bloggers)
+    path('bloggers', views.bloggers),
+    path('userpage', views.userpage)
 ]
