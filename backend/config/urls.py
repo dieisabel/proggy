531a5f531f87f62c41ca0b1aa69d93b31c17b410
dieisabel <konstkadyr@gmail.com>
2021-03-12 13:21:26 +0200

    Добавил маршрут к register
    Добавил в главный urls.py маршрут к вьюшке для регистрации register

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from accounts.views import register
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('register', register, name='accounts-register')
 ]
