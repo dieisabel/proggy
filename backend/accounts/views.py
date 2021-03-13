1be1d79f2f0a9cbfae86dc39dcdb29649dcbe013
dieisabel <konstkadyr@gmail.com>
2021-03-13 19:28:54 +0200

    Добавил обработку данных с формы регистрации
    При POST запросе к вьюшке register данные с формы обрабатываются и
    создаётся пользователь, в случаи успешной валидации данных
    Также производится перенаправление к главной страницы для блогов, где
    выводится flash сообщение об успешном создании нового пользователя

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib import messages
+
 
 def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created {username}')
+            form.save()
+            return redirect('/')
     form = UserCreationForm()
     return render(request, 'accounts/registration.html', {'form': form})
