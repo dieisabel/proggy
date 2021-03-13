55b908e628b094d6d44bcb52a4a9d948aa3f06ae
dieisabel <konstkadyr@gmail.com>
2021-03-13 19:57:59 +0200

    Добавил имена к маршрутам приложения blog
    Имена имеют следующую конвенцию: <приложение>-<вьюшка>

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blogs),
-    path('bloggers', views.bloggers),
-    path('userpage', views.userpage)
+    path('', views.blogs, name='blog-blogs'),
+    path('bloggers', views.bloggers, name='blog-bloggers'),
+    path('userpage', views.userpage, name='blog-userpage')
 ]
