4e54e14261a8d77bf91a7e86b9e5efbea3581a56
dieisabel <konstkadyr@gmail.com>
2021-03-14 15:14:18 +0200

    Добавил вьюшку для выхода из аккаунта
    Использовал встроенную вьюшку LogoutView из django.contrib.auth

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('login',
          auth_views.LoginView.as_view(template_name='accounts/login.html'),
          name='accounts-login'),
+    path('logout',
+         auth_views.LogoutView.as_view(template_name='accounts/logout.html'),
+         name='accounts-logout'),
 ]
