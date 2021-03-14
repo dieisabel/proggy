f1e718360c38eb6bde927f13d67ffd73294a7d8c
dieisabel <konstkadyr@gmail.com>
2021-03-14 14:33:55 +0200

    Добавил вьюшку для входа в аккаунт
    Для входа использовал уже готовую вьюшку LoginView из
    django.contrib.auth

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from django.contrib.auth import views as auth_views
+
 from accounts.views import register
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('register', register, name='accounts-register')
+    path('register', register, name='accounts-register'),
+    path('login',
+         auth_views.LoginView.as_view(template_name='accounts/login.html'),
+         name='accounts-login'),
 ]
