8ff2f4e5de390f7ad3a706ead899a156ce3ef4f3
Konstantine <konstkadyr@gmail.com>
2021-03-21 15:12:03 +0200

    fix #29
    Добавил поле ввода почты для регистрации и обновил вьюшку

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 from django.contrib import messages
 
+from .forms import UserRegistrationForm
+
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created {username}')
             form.save()
             return redirect('blog-blogs')
-    form = UserCreationForm()
+    form = UserRegistrationForm()
     return render(request, 'accounts/registration.html', {'form': form})
 
 
