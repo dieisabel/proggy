b81b00350a771b79906493003dadf84a4bf354b7
dieisabel <konstkadyr@gmail.com>
2021-03-25 17:50:21 +0200

    Добавил форму UserUpdateForm
    Форма предназначена для изменения данных пользователя, а именно:
    - почта
    - first_name
    - last_name

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -13,3 +13,12 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
