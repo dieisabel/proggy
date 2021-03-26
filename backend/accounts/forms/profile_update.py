226a4a41200673897756421d6c3374d5cf3f50d2
dieisabel <konstkadyr@gmail.com>
2021-03-26 16:33:24 +0200

    Обновил форму ProfileUpdate для новой модели

--- backend/accounts/forms/profile_update.py
+++ backend/accounts/forms/profile_update.py
@@ -15,7 +15,19 @@ from accounts.models import Profile
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = [
+            'image',
+            'bio',
+            'birthday',
+            'twitter_url',
+            'custom_url',
+            'gender',
+        ]
+        widgets = {
+            'birthday': forms.DateInput(
+                format=("%d/%m/%Y"),
+                attrs={'placeholder': 'Select a date', 'type': 'date'})
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -23,7 +35,12 @@ class ProfileUpdateForm(forms.ModelForm):
         self.helper = FormHelper()
         self.helper.form_tag = False
         self.helper.layout = Layout(
-            Field('image'),
+            Field('gender', autocomplete='off'),
+            Field('bio', autocomplete='off'),
+            Field('birthday', autocomplete='off'),
+            Field('twitter_url', autocomplete='off'),
+            Field('custom_url', autocomplete='off'),
+            Field('image', autocomplete='off'),
             ButtonHolder(
                 Submit('submit', 'Update')
             )
