94e7bac4db998e457fe9582282c1778060a45b90
dieisabel <konstkadyr@gmail.com>
2021-03-26 16:24:01 +0200

    Добавил новые поля в модель Profile
    Теперь Profile содержит в себе все необходимые поля
    
    Добавил:
    - gender
        пол
    - bio
        биография
    - birthday
        день рождения
    - twitter_url
        страничка в твитере
    - custom_url
        ссылка на свой сайт
    
    Добавил миграции для обновлённой модели

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,10 +3,23 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    GENDER_CHOICES = [
+        ('F', 'Female'),
+        ('M', 'Male'),
+        ('?', 'Don\'t worry about it')
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField('аватарка',
                               default='default/default_profile_pic.png',
                               upload_to='profile_pics')
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        default='?')
+    bio = models.TextField('биография', blank=True)
+    birthday = models.DateField('день рождения', blank=True, null=True)
+    twitter_url = models.URLField('твиттер', blank=True)
+    custom_url = models.URLField('своя ссылка', blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
