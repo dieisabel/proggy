42a1f5faf2707abe6e70117176e0bdb3631d54d1
dieisabel <konstkadyr@gmail.com>
2021-03-29 17:46:31 +0300

    Добавил в Post краткое описание
    Добавил в модель Post поле brief_description, которое представляет из
    себя CharField
    Добавил миграции

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField('заголовок', max_length=255)
+    brief_description = models.CharField(
+        'краткое описание',
+        max_length=1000,
+        default=f'Learn about {title} in this post!')
     content = models.TextField('текст')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField('дата и время создания',
