283bf28cde963fe2f560561f98e643f405512a94
dieisabel <konstkadyr@gmail.com>
2021-04-20 20:39:15 +0300

    Добавил функционал поиска блогов по тегу
    Поиск блогов по тегам необходим для отображения пользователю блогов,
    которые соответствуют его интересам
    
    Реализовано при помощи изменения коллекции блогов, которые будут
    отображаться пользователю. Коллекцияфильтруется исходя из
    url query string параметра tag_name, а если
    параметр не указан, то идёт отображение всех недавних блогов

--- backend/blog/views/blogs.py
+++ backend/blog/views/blogs.py
@@ -10,4 +10,13 @@ class BlogsView(ListView):
     model = Post
     template_name = 'blog/main/blogs.html'
     context_object_name = 'blogs'
-    ordering = ['-created_at']
+    ordering = '-created_at'
+
+    def get_queryset(self):
+        tag_name = self.request.GET.get('tag_name') or None
+        if tag_name:
+            return Post.objects.all(
+                ).filter(tags__name=tag_name
+                ).order_by(self.ordering)
+        return Post.objects.all(
+            ).order_by(self.ordering)
