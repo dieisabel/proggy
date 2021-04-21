bc121c3cc382ea25fd7f39576a71a6e2752007ec
dieisabel <konstkadyr@gmail.com>
2021-04-21 18:10:55 +0300

    Добавил обработку POST метода в BlogDetailView
    Добавление комментариев к блогу осуществляется с помощью формы, которая
    отправляет POST запрос к тому же контроллеру, который и обрабатывает GET
    запрос
    
    Возможны какие-либо проблемы с безопасностью, так как данные с формы
    никак не валидируются

--- backend/blog/views/blog.py
+++ backend/blog/views/blog.py
@@ -3,10 +3,20 @@ __all__ = ['BlogDetailView']
 
 from django.views.generic import DetailView
 
+from django.shortcuts import redirect
+
 from blog.models import Post
+from blog.models import Comment
 
 
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/main/blog.html'
     context_object_name = 'blog'
+
+    def post(self, request, pk):
+        comment_content = self.request.POST.get('comment_content')
+        Comment.objects.create(author=self.request.user,
+                               post=Post.objects.all().filter(id=pk).first(),
+                               content=comment_content)
+        return redirect('blog-blog', pk)
