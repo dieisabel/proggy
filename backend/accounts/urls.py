48428adce8af568cb125f3425631b69717a9ee88
dieisabel <konstkadyr@gmail.com>
2021-04-23 21:32:00 +0300

    Добавил контроллеры для сброса пароля
    За разные этапы сброса пароля отвечают разные контроллеры:
    
    - PasswordResetView
        Отправная точка. Пользователь вводи почту, на которую придёт письмо
    - PasswordResetDoneView
        При отправке письма выведется сообщение о том, что письмо отправлено
    - PasswordResetConfirmView
        После подтверждение производит редирект на страницу с
        вводом нового пароля. Принимает два параметра uidb64 и token
        uidb64 - первичный ключ, закодированный при помощи base64
        token - уникально сгенерированный токен, который актуален
        ограниченное количество времени
    - PasswordResetCompleteView
        Вывод сообщения после успешной смены пароля
    
    feature: #61

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from django.contrib.auth import views as auth_views
 
@@ -21,4 +21,17 @@ urlpatterns = [
          ProfileBlogsView.as_view(), name='accounts-profile-blogs'),
     path('profile/<str:username>/edit',
          ProfileEditView.as_view(), name='accounts-profile-edit'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+            template_name='accounts/main/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done',
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/main/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password_reset/confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/main/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password_reset/complete',
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/main/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
