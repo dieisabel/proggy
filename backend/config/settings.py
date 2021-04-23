3481eda5a4990887a740823f7f8f2e4d5cbbb5b5
dieisabel <konstkadyr@gmail.com>
2021-04-23 21:17:38 +0300

    Добавил настройки для отправки email
    Используется smtp сервер gmail, а данные для входа в целях
    безопасности должны быть переменными окружения
    
    feature: #61

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,3 +94,10 @@ LOGIN_REDIRECT_URL = 'blog-blogs'
 LOGIN_URL = 'accounts-login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
