f1f1983379540773aa5801610585d018dd346fe3
dieisabel <konstkadyr@gmail.com>
2021-04-25 12:43:58 +0300

    Расспределил в папку шаблоны для сброса пароля
    Думаю, что это хорошее решение. Также обновил маршруты, указав новые
    пути к шаблонам
    
    refactor: #67

--- backend/accounts/urls/password_reset.py
+++ backend/accounts/urls/password_reset.py
@@ -8,18 +8,18 @@ from django.contrib.auth import views as auth_views
 password_reset_urlpatterns = ([
     path('',
          auth_views.PasswordResetView.as_view(
-             template_name='accounts/main/password_reset.html'),
+             template_name='accounts/main/password_reset/initial.html'),
          name='initial'),
     path('done',
          auth_views.PasswordResetDoneView.as_view(
-             template_name='accounts/main/password_reset_done.html'),
+             template_name='accounts/main/password_reset/done.html'),
          name='done'),
     path('confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
-             template_name='accounts/main/password_reset_confirm.html'),
+             template_name='accounts/main/password_reset/confirm.html'),
          name='confirm'),
     path('complete',
          auth_views.PasswordResetCompleteView.as_view(
-             template_name='accounts/main/password_reset_complete.html'),
+             template_name='accounts/main/password_reset/complete.html'),
          name='complete'),
 ], 'password_reset')
