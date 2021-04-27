b415ca8e443c4fe13d4862434b64d05d5a1a6a1f
dieisabel <konstkadyr@gmail.com>
2021-04-27 19:19:48 +0300

    Использовал форму для обработки комментария
    fix: #68

--- backend/blog/views/blog.py
+++ backend/blog/views/blog.py
@@ -7,6 +7,7 @@ from django.shortcuts import redirect
 
 from blog.models import Post
 from blog.models import Comment
+from blog.forms import CommentForm
 
 
 class BlogDetailView(DetailView):
@@ -15,8 +16,9 @@ class BlogDetailView(DetailView):
     context_object_name = 'blog'
 
     def post(self, request, pk):
-        comment_content = self.request.POST.get('comment_content')
-        Comment.objects.create(author=self.request.user,
-                               post=Post.objects.all().filter(id=pk).first(),
-                               content=comment_content)
+        comment_form = CommentForm(self.request.POST)
+        if comment_form.is_valid():
+            Comment.objects.create(author=self.request.user,
+                                   post=Post.objects.all().filter(id=pk).first(),
+                                   content=comment_form.cleaned_data.get('comment_content'))
         return redirect('blog:posts:post', pk)
