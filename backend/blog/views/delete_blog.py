6febade1079ab274021774b24f8bcb31819ec15a
dieisabel <konstkadyr@gmail.com>
2021-04-27 19:57:37 +0300

    Добавил проверку при удалении поста
    Проверяется, является ли пользователь блоггером и автором
    
    fix: #65

--- backend/blog/views/delete_blog.py
+++ backend/blog/views/delete_blog.py
@@ -4,14 +4,18 @@ __all__ = ['DeleteBlogView']
 from django.urls import reverse_lazy
 
 from django.views.generic import DeleteView
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 from blog.models import Post
 
 
-@method_decorator(login_required, name='dispatch')
-class DeleteBlogView(DeleteView):
+class DeleteBlogView(UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/main/post_crud/delete_post.html'
     success_url = reverse_lazy('blog:blogs')
+
+    def test_func(self):
+        if self.get_object().author.username != self.request.user.username \
+                or not self.request.user.groups.filter(name='blogger').exists():
+            return False
+        return True
