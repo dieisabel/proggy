5fd1c92335b257bd90384b7c9bc65056f72d0dd7
dieisabel <konstkadyr@gmail.com>
2021-04-30 18:48:46 +0300

    Настроил модели для админки приложения accounts
    Также вместе с информацией об модели отображается и картинка профиля
    
    feature: #63

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,6 +1,25 @@
+__all__ = ['ProfileAdmin']
+
+
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Profile
 
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Main', {
+            'fields': ('user', 'bio', 'image', 'get_image', 'gender', 'birthday')
+        }),
+        ('Contacts', {
+            'fields': ('twitter_url', 'custom_url')
+        }),
+    )
+    readonly_fields = ('user', 'get_image')
+
+    def get_image(self, instance):
+        return mark_safe(f'<img src="{instance.image.url}" width=100 height=100 >')
+
+    get_image.short_description = 'Изображение'
